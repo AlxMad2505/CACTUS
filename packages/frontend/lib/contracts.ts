560188d22808333461dd1fd44392cc40e6da7468
AlxMad2505 <agp90072@gmail.com>
2026-09-24 20:25:09 -0600

    fix 2

--- packages/frontend/lib/contracts.ts
+++ packages/frontend/lib/contracts.ts
@@ -1,6 +1,6 @@
-import BitacoraInmueble from "../../blockchain/artifacts/contracts/BitacoraInmueble.sol/BitacoraInmueble.json";
-import MockIcmMessenger from "../../blockchain/artifacts/contracts/mocks/MockIcmMessenger.sol/MockIcmMessenger.json";
-import IcmPropertyPortal from "../../blockchain/artifacts/contracts/IcmPropertyPortal.sol/IcmPropertyPortal.json";
+import BitacoraInmueble from "./abis/BitacoraInmueble.sol/BitacoraInmueble.json";
+import MockIcmMessenger from "./abis/mocks/MockIcmMessenger.sol/MockIcmMessenger.json";
+import IcmPropertyPortal from "./abis/IcmPropertyPortal.sol/IcmPropertyPortal.json";
 
 export const ABIs = {
   BitacoraInmueble: BitacoraInmueble.abi,
